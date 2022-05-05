7d8be0906f9ebfe35652b51cbbde90246b0bdfbb
duanlingbosjtu <duanlingbosjtu@sjtu.edu.cn>
2022-05-05 13:40:44 +0800

    Site updated: 2022-05-05 13:40:44

--- generate/GeneraterSplit.py
+++ generate/GeneraterSplit.py
@@ -24,8 +24,8 @@ for i in range(1,len(Type)):
 
 for tp in set1:
     html = f'---\ntitle: {tp}\n';
-    html += 'cover: /src/640.jpeg\n---\n\n<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <meta http-equiv="X-UA-Compatible" content="IE=edge">\n     <meta name="viewport" content="width=device-width, initial-scale=1.0">\n    <title>JIers</title>\n<style>.box{width: 200px;height: 150px;margin-top: 0px;margin-bottom: 0px;}.item{width: 700px;height: 200px;}</style>\n</head>\n<body>\n<ul class="list" style="list-style: none;">';
-    
+    html += 'cover: /src/640.jpeg\n---\n\n<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="UTF-8">\n    <meta http-equiv="X-UA-Compatible" content="IE=edge">\n     <meta name="viewport" content="width=device-width, initial-scale=1.0">\n    <title>JIers</title>\n<style>@media screen and (min-width:540px){.box{width: 200px;height: 150px;margin-top: 0px;margin-bottom: 0px;float: left;}.url{margin-left: 50px;}.item{width: 700px;height: 200px;}.note{font-size: 12pt; font-style: italic; display: flex; margin-top: 30px;margin-left: 300px;}}@media screen and (max-width:540px) {.box{width: 330px;height: 150px;margin-top: 0px;margin-bottom: 0px;}.item{width: 330px;height: 330px;}.note{font-size: 10pt; font-style: italic; display: flex; margin-left: 0px;}}</style>\n</head>\n<body>\n<ul class="list" style="list-style: none;">';
+
     for i in range(1,len(Type)):
         if Type[i].value!=tp:
             continue;
@@ -34,11 +34,12 @@ for tp in set1:
         Res=requests.get(Pic[i].value,headers=headers);
         with open(f'../src/{i}.jpg','wb') as f:
             f.write(Res.content);
-        tmp+=f'<div class="box" style="background: url(/src/{i}.jpg) no-repeat; background-size: 100% 100%; float: left;"></div>\n';
+        tmp+=f'<div class="box" style="background: url(/src/{i}.jpg) no-repeat; background-size: 100% 100%;"></div>\n';
         tmp+='<div class="container">\n';
-        tmp+=f'<a href="{Url[i].value}" style="font-size: 15pt; margin-left: 50px;">{Title[i].value}</a>\n';
-        tmp+=f'<div style="font-size: 12pt; font-style: italic; display: flex; margin-top: 30px;">\n';
-        tmp+=f'<div style="margin-left: 250px;">发布编号：{Num[i].value}</div>\n';
+        tmp+=f'<a href="{Url[i].value}" style="font-size: 15pt;" class="url">{Title[i].value}</a>\n';
+        tmp+=f'<div class="note">\n';
+        tmp+=f'<div>信息来源：{Type[i].value}</div>\n';
+        tmp+=f'<div>发布编号：{Num[i].value}</div>\n';
         tmp+=f'<div>发布日期：{Date[i].value}</div></div></div></li>\n';
         html+=tmp;
     
